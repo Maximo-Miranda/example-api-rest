75cc9428eae095fadb178676cbd5e1b929d0b16a
maximomiranda <maximomirandah@gmail.com>
2019-11-21 18:00:15 -0500

    Initial commit and create project structure.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Maximo-Miranda/example-api-rest
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// main ...
+func main(){
+
+	fmt.Println("Example APIRest service.")
+}
